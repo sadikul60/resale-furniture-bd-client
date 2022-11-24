3e6261663394c659b121adece12edb34f2ec897c
mdsadikul <mdsadikulislam891@gamil.com>
2022-11-24 08:06:08 +0600

    Added Footer

--- src/routes/Routes/Routes.js
+++ src/routes/Routes/Routes.js
@@ -9,6 +9,10 @@ const router = createBrowserRouter([
         element: <Main></Main>,
         children: [
             {
+                path: '/',
+                element: <Home></Home>
+            },
+            {
                 path: '/home',
                 element: <Home></Home>
             },
