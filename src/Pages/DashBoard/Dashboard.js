1b591c4feec64234aabda5f039063b744b02ac39
mdsadikul <mdsadikulislam891@gamil.com>
2022-11-27 20:02:31 +0600

    Edite dashboard text

--- src/Pages/DashBoard/Dashboard.js
+++ src/Pages/DashBoard/Dashboard.js
@@ -5,7 +5,12 @@ const Dashboard = () => {
     return (
         <div className='w-11/12 mx-auto mt-48'>
             <img className='w-56 h-56 mx-auto rounded-full' src={img} alt="" />
-            <h1 className='text-center text-4xl font-bold'>Please! Go to any Route of your choice.</h1>
+            <div className='hidden lg:block'>
+                <h1 className='text-center text-4xl font-bold'>Please! Go to any Route of your choice.</h1>
+            </div>
+            <div className='lg:hidden'>
+                <h1 className='text-center text-4xl font-bold'>Please! Click On MENU.</h1>
+            </div>
         </div>
     );
 };
